181b42fe603976c9b671f670550ac2b4cac6710c
mylitvinov <vinchester30@mail.ru>
2021-08-14 17:16:26 +0300

    edit dir

--- Function/Sum numbers.js
+++ Function/Sum numbers.js
@@ -1,12 +1,14 @@
 const sum = (str) => {
     let result = 0;
-    for (let i = 0; i < length(str); i += 1) {
+    for (let i = 0; i < str.length; i += 1) {
       result += Number(str[i]);
-    }
+    }1
   
     return result;
   };
   
+  console.log(sum('123'))
+
   const addDigits = (num) => {
     let result = num;
     while (result >= 10) {
@@ -15,4 +17,6 @@ const sum = (str) => {
   
     return result;
   };
+
+  console.log(addDigits(3456)); // 9 //  3+4+5+6 = 18 = 1+8 
   
