08bcf6446896d25649ede29ae05a2bf1c521e46a
mylitvinov <vinchester30@mail.ru>
2021-08-21 15:34:43 +0300

    edit LearnJS_Tasks/Map and Set

--- Map and Set/Uniq elements array.js
+++ Map and Set/Uniq elements array.js
@@ -4,9 +4,6 @@
 // Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся 
 // значений массива arr.
 
-function unique(arr) {
-    /* ваш код */
-  }
   
   let values = ["Hare", "Krishna", "Hare", "Krishna",
     "Krishna", "Krishna", "Hare", "Hare", ":-O"
@@ -17,3 +14,24 @@ function unique(arr) {
   function unique(arr) {
     return Array.from(new Set(arr));
   }
+
+  /*
+  
+function unique(arr) {
+    let result = [];
+  
+    for (let str of arr) {
+      if (!result.includes(str)) {
+        result.push(str);
+      }
+    }
+  
+    return result;
+  }
+  
+  let strings = ["кришна", "кришна", "харе", "харе",
+    "харе", "харе", "кришна", "кришна", ":-O"
+  ];
+  
+  console.log( unique(strings) ); // кришна, харе, :-O
+  */
