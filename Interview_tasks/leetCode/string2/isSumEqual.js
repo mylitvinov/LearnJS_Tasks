226aeb4804d3a42603986fad8a41a0cf7c35e051
mylitvinov <vinchester30@mail.ru>
2021-09-21 19:42:52 +0300

    isSumEqual.js

---
+++ Interview_tasks/leetCode/string2/isSumEqual.js
@@ -0,0 +1,37 @@
+/*
+Example 1:
+
+Input: firstWord = "acb", secondWord = "cba", targetWord = "cdb"
+Output: true
+Explanation:
+The numerical value of firstWord is "acb" -> "021" -> 21.
+The numerical value of secondWord is "cba" -> "210" -> 210.
+The numerical value of targetWord is "cdb" -> "231" -> 231.
+We return true because 21 + 210 == 231.
+Example 2:
+
+Input: firstWord = "aaa", secondWord = "a", targetWord = "aab"
+Output: false
+Explanation: 
+The numerical value of firstWord is "aaa" -> "000" -> 0.
+The numerical value of secondWord is "a" -> "0" -> 0.
+The numerical value of targetWord is "aab" -> "001" -> 1.
+We return false because 0 + 0 != 1.
+Example 3:
+
+Input: firstWord = "aaa", secondWord = "a", targetWord = "aaaa"
+Output: true
+Explanation: 
+The numerical value of firstWord is "aaa" -> "000" -> 0.
+The numerical value of secondWord is "a" -> "0" -> 0.
+The numerical value of targetWord is "aaaa" -> "0000" -> 0.
+We return true because 0 + 0 == 0.
+
+*/
+
+const isSumEqual = function(firstWord, secondWord, targetWord) {
+  const wordToInt = (word) => parseInt([...word].reduce((a,c) => a + (c.charCodeAt(0) - 'a'.charCodeAt(0)), ''))
+ 
+  return wordToInt(firstWord) + wordToInt(secondWord) === wordToInt(targetWord);
+  
+};
