4542a43d927cee068353787469e985fcb6235998
mylitvinov <vinchester30@mail.ru>
2021-11-01 12:17:07 +0300

    destructuring.js

--- Interview_tasks/freeCodeCamp/destructuring.js
+++ Interview_tasks/freeCodeCamp/destructuring.js
@@ -12,3 +12,14 @@ const {today,tomorrow} = HIGH_TEMPERATURES
 
 console.log(today,tomorrow)
 
+
+// Only change code below this line
+  
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+const {today:highToday , tomorrow:highTomorrow}=HIGH_TEMPERATURES
+
+console.log(highToday ,highTomorrow)
+
+// Only change code above this line
